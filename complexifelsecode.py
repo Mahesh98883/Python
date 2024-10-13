b4ca7aea6f99809ac47a14aaa78ace9cd9a3a0a4
Mahesh Varigonda <Mahesh>
2024-10-13 18:43:04 +0530

    commit message

---
+++ complexifelsecode.py
@@ -0,0 +1,9 @@
+a=int(input("what is your kd?"))
+if a==1:#if it satisfies 1st condition it won't run another conditions below
+    print("Noob")
+elif a==2:#elif is shortform of elseif
+    print("good")
+elif a>3:
+    print("pro")
+else:
+    print("hatt ja")
--- ifproblem.py
+++ ifproblem.py
@@ -1,8 +1,23 @@
+# Calculate shipping charges for a shopper
+
+# * Ask the user to enter the amount for their total purchase
+
+# If their total is under $50 add $10, otherwise shipping is free
+
+# Tell the user their final total including shipping costs and forma the number so it looks like a monetary value
+
+# Don't forget to test your solution with
+
+# -a value 50
+
+# - a value 50
+
+# -a value of exactly 50
 ShippingCharges=int(input())
 b=ShippingCharges+10
 if(ShippingCharges<50):
     print("extra $10 need to be paid for shipping under $50")
-    print("$"+str(b)+"only")
+    print("$"+str(b)+"only")#using str(b) for converting int to str for printing output with dollar symbol
 else:
     print("$"+str(ShippingCharges)+"only")
 
